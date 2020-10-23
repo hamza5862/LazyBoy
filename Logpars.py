6d124a3d4298ebdb0038e8e949ee52980d9657d7
hamza5862 <hamza.5862@gmail.com>
2020-10-22 20:55:00 -0400

    need work on all attacks

--- Logpars.py
+++ Logpars.py
@@ -11,12 +11,14 @@ import os
 
 
 class fun:
+    doc=""
     find_string=""
     filename=""
     ip_address= ""
     event_no =[]
     counter=1
     line_number=[]
+    find=''
     count=0
     ipdst=[]
     ipsrc=[]
@@ -49,9 +51,12 @@ def tables():
     x = PrettyTable()
     x.field_names = ["Ip source Addesses","Ip dst Addesses", " dst Ports","Services", "Kind of Attacks", "Attack examples","Total Attacks"]
     print("Printing Report...")
-    for i in range(3):
-        x.add_row([fun.attackip[i],fun.attackdst[i],fun.attackport[i] , servers.services(fun.attackport[i]),fun.filename, fun.attacktype[i][5:100],fun.count])
+    # for i in range(3):
+    x.add_row([fun.attackip[0],fun.attackdst[0],fun.attackport[0] , servers.services(fun.attackport[0]),fun.filename, fun.attacktype[0][5:100],' '])
+    x.add_row([fun.attackip[1],fun.attackdst[1],fun.attackport[1] , servers.services(fun.attackport[1]),fun.filename, fun.attacktype[1][5:100],fun.count])
+    x.add_row([fun.attackip[2],fun.attackdst[2],fun.attackport[2] , servers.services(fun.attackport[2]),fun.filename, fun.attacktype[2][5:100],' '])
     print(x) 
+
 
 
 
@@ -76,15 +81,50 @@ def userinput():
     print("What kind of attack are you looking for? \n i.e: (1)LFI   -  (2)RFI - (3)BufferOverflow (4)All kind of attacks\n\n please input your chooice here: " )
     print("input recived...")
     # if int(input()) < 
-    kindOfAttack(int(input()))
+    pickattack(int(input()))
 
     
-    
+
+def pickattack(x):
+    if x == 4:
+        print("gets to if ststs")
+        for i in range(3):
+            allattacks(i+1)
+            # break 
+    else:
+        print("do better coding")
+        kindOfAttack(x)
+
+def allattacks(x):
+        print("hahaahaha"+ str(x))
+        find=''
+        if x ==1:
+            lfi= "/../../"
+            fun.filename= "Local_file_inclusion_(LFI)"  # create a "summary file" 
+            find = lfi
+            readfile()
+        elif x ==2 :
+            rfi= "/?="
+            fun.filename= "Remote File Inclusion (RFI)"    # modfi "summary file"
+            find = rfi
+            readfile()
+        elif x ==3 :
+            buffr= r"(\\x\D)"
+            find = buffr
+            fun.filename= "Buffer OverFlow Attack (BOF)"   # modify "summary file"    
+            readfile()
+        else:
+            print ("dont be stupid !!!!!!")
+            exit()
+        fun.find_string = find
+        print(fun.filename)
+        # readfile()
+        
 # few attack types
- 
 def kindOfAttack(x):
     find=''
     print ("Encoding the Input...")
+
     if x ==1:
         lfi= "/../../"
         fun.filename= "Local_file_inclusion_(LFI)"  # create a "summary file" 
@@ -96,29 +136,17 @@ def kindOfAttack(x):
     elif x ==3 :
         buffr= r"(\\x\D)"
         find = buffr
-        fun.filename= "Buffer OverFlow Attack (BOF)"   # modify "summary file"
-    elif x ==4:
-        i =0
-        for i in range(3):
-            i += 1
-            x =i
-            print(i) 
-            # kindOfAttack(readfile())
-            break
-        
-
+        fun.filename= "Buffer OverFlow Attack (BOF)"   # modify "summary file"    
     else:
         print ("dont be stupid !!!!!!")
         exit()
-    
-
     fun.find_string = find
     print(fun.filename)
     readfile()
-   
-def sysarg(x):
-    print(f"Finding {x}...")
-    readfile()
+    
+
+
+
 
 # Import data from log file
 def readfile():
@@ -129,7 +157,7 @@ def readfile():
     attack_type=[]
     attack_type= []
     print ("Opening the log file....")
-    f = open("/home/kali/Documents/project/http1.log", "r")
+    f = open(fun.doc, "r")
     # counter=0
     file = f.readlines()
     # print(file[9])
@@ -156,7 +184,6 @@ def readfile():
         fun.attackport = word_count(attack_dstPort)
         fun.attacktype = word_count(attack_type)
     makefile()
-    print ("Parsing the data...")
     getcount()
 
 def word_count(str):
@@ -202,16 +229,19 @@ def makefile():
     ipsrcfile.close() 
     print("File created. Closing it now")
 
+
 if len(sys.argv) > 1 :
     
-    sysarg(sys.argv[1])
+    fun.doc=str(sys.argv[1])
+    print(sys.argv[0])
+
 
 else:
-    userinput()
-    # makefile(fun.ipdst)
-    # print ("working on Bash now...........")
-    # os.system("./bash")
-    tables()
-    print(color.BOLD+f"\n\n\n\n ********---->  Please look at file {fun.filename}.txt for line numbers.  <------********\n\n\n")
+    print 
+    print("else statemetn")
+    fun.doc="http1.log"
 
 
+userinput()
+tables()
+print(color.BOLD+f"\n\n\n\n ********---->  Please look at file {fun.filename}.txt for line numbers.  <------********\n\n\n")
