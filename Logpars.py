48e8811a845a57e134642fb1fd045b93efc2d072
hamza5862 <hamza.5862@gmail.com>
2020-10-24 13:39:22 -0400

    buffer table() needs work

--- Logpars.py
+++ Logpars.py
@@ -16,8 +16,8 @@ class fun:
     filename=""
     ip_address= ""
     event_no =[]
-    counter=1
-    line_number=[]
+    
+    # line_number=[]
     find=''
     count=0
     ipdst=[]
@@ -52,14 +52,10 @@ def tables():
     x.field_names = ["Ip source Addesses","Ip dst Addesses", " dst Ports","Services", "Kind of Attacks", "Attack examples","Total Attacks"]
     print("Printing Report...")
     # for i in range(3):
-    x.add_row([fun.attackip[0],fun.attackdst[0],fun.attackport[0] , servers.services(fun.attackport[0]),fun.filename, fun.attacktype[0][5:100],' '])
-    x.add_row([fun.attackip[1],fun.attackdst[1],fun.attackport[1] , servers.services(fun.attackport[1]),fun.filename, fun.attacktype[1][5:100],fun.count])
-    x.add_row([fun.attackip[2],fun.attackdst[2],fun.attackport[2] , servers.services(fun.attackport[2]),fun.filename, fun.attacktype[2][5:100],' '])
+    x.add_row([fun.attackip[0],fun.attackdst[0],fun.attackport[0] , servers.services(fun.attackport[0]),fun.filename, fun.attacktype[0][:],' '])
+    x.add_row([fun.attackip[1],fun.attackdst[1],fun.attackport[1] , servers.services(fun.attackport[1]),fun.filename, fun.attacktype[1][:],fun.count])
+    x.add_row([fun.attackip[2],fun.attackdst[2],fun.attackport[2] , servers.services(fun.attackport[2]),fun.filename, fun.attacktype[2][0:10],' '])
     print(x) 
-
-
-
-
 
 
 class color:
@@ -81,73 +77,44 @@ def userinput():
     print("What kind of attack are you looking for? \n i.e: (1)LFI   -  (2)RFI - (3)BufferOverflow (4)All kind of attacks\n\n please input your chooice here: " )
     print("input recived...")
     # if int(input()) < 
-    pickattack(int(input()))
-
-
-def pickattack(x):
-    if x == 4:
-        print("gets to if ststs")
+    inputnumber=int(input())
+    if inputnumber == 4:
         for i in range(3):
-            allattacks(i+1)
-           # readfile()
-            
-	
-   # else:
-        #print("do better coding")
-       # kindOfAttack(x)
+            temp = i+1
+            kindOfAttack(temp)
+            tables()
+            print(color.BOLD+f"\n\n\n\n ********---->  Please look at file {fun.filename}.txt for line numbers.  <------********\n\n\n")
 
-def allattacks(x):
-        print("hahaahaha"+ str(x))
-        find=''
-        if x ==1:
-            lfi= "/../../"
-            fun.filename= "Local_file_inclusion_(LFI)"  # create a "summary file" 
-            find = lfi
-            readfile()
-            tables()
+    else:
+        kindOfAttack(inputnumber)
+        tables()
+        print(color.BOLD+f"\n\n\n\n ********---->  Please look at file {fun.filename}.txt for line numbers.  <------********\n\n\n")
 
-        elif x ==2 :
-            rfi= "/?="
-            fun.filename= "Remote File Inclusion (RFI)"    # modfi "summary file"
-            find = rfi
-            readfile()
-            tables()
-        elif x ==3 :
-            buffr= r"(\\x\D)"
-            find = buffr
-            fun.filename= "Buffer OverFlow Attack (BOF)"   # modify "summary file"    
-            readfile()
-            tables()
-        else:
-            print ("dont be stupid !!!!!!")
-            exit()
-        fun.find_string = find
-        print(fun.filename)
-        # readfile()
-        
+    
+    
+
+       
 # few attack types
-#def kindOfAttack(x):
-   # find=''
-  #  print ("Encoding the Input...")
+def kindOfAttack(x):
+   print ("Encoding the Input...")
 
-   # if x ==1:
-       # lfi= "/../../"
-       # fun.filename= "Local_file_inclusion_(LFI)"  # create a "summary file" 
-     #   find = lfi
-   # elif x ==2 :
-       # rfi= "/?="
-       # fun.filename= "Remote File Inclusion (RFI)"    # modfi "summary file"
-#        find = rfi
-#    elif x ==3 :
-#        buffr= r"(\\x\D)"
-#        find = buffr
-#        fun.filename= "Buffer OverFlow Attack (BOF)"   # modify "summary file"    
-#    else:
-#        print ("dont be stupid !!!!!!")
-#        exit()
-#   # fun.find_string = find
-   # print(fun.filename)
-   # readfile()
+   if x ==1:
+       lfi= "/../../"
+       fun.filename= "Local_file_inclusion_(LFI)"  # create a "summary file" 
+       fun.find = lfi
+   elif x ==2 :
+       rfi= "/?=http"
+       fun.filename= "Remote File Inclusion (RFI)"    # modfi "summary file"
+       fun.find = rfi
+   elif x ==3 :
+       buffr= r"(\\x\D)"
+       fun.find = buffr
+       fun.filename= "Buffer OverFlow Attack (BOF)"   # modify "summary file"    
+   else:
+       print ("dont be stupid !!!!!!")
+       exit()
+   print(fun.filename)
+   readfile()
     
 
 
@@ -163,18 +130,20 @@ def readfile():
     attack_type= []
     print ("Opening the log file....")
     f = open(fun.doc, "r")
-    # counter=0
+    counter=0
     file = f.readlines()
-    # print(file[9])
-    # data=[]
     print ("Reading the log file....")
     linenumbercounter=0
+    line_number=[]
+    ipsrcfile = open(fun.filename, "a+")
     for line in file:
         # if fewlines < 11:
         linenumbercounter+=1
-        if re.search(fun.find_string, str(line)):
+        # if re.search(fun.find_string, str(line)):
+        if re.search(fun.find, str(line)):
             fun.count+=1
-            fun.line_number.append(linenumbercounter)
+            counter+=1
+            line_number.append(linenumbercounter)
 
             entry_details = re.split(r'\t+', str(line))
             attack_source.append(entry_details[2])
@@ -188,8 +157,15 @@ def readfile():
         fun.attackdst =word_count(attack_dst)
         fun.attackport = word_count(attack_dstPort)
         fun.attacktype = word_count(attack_type)
-    makefile()
+        # print(str(attack_type[0:100])+"\n\n")
+
+    for i in range(counter):\
+        ipsrcfile.write("\n LineNumber:"+ str(line_number[i])+"\nSource:"+str(attack_source[i]) + "\nDst Server:" + str(attack_dst[i])+ " -->  "+ str(attack_dstPort[i])+ "\nAttack:"+str(attack_type[i])+"\n\n\n")
+
+
+        
     getcount()
+
 
 def word_count(str):
     counts = dict()
@@ -225,14 +201,6 @@ def attackcount(str):
     
 
 
-def makefile():
-
-    print("Creating a file...")
-
-    ipsrcfile = open(fun.filename, "a+")
-    ipsrcfile.write("\n"+ str(fun.line_number))
-    ipsrcfile.close() 
-    print("File created. Closing it now")
 
 
 if len(sys.argv) > 1 :
@@ -248,5 +216,4 @@ else:
 
 
 userinput()
-tables()
-print(color.BOLD+f"\n\n\n\n ********---->  Please look at file {fun.filename}.txt for line numbers.  <------********\n\n\n")
+
